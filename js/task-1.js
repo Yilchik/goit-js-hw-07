5ec4657f2aa737f5df73d4fb085ff3341714b609
Yilchik <yulyayulia2@gmail.com>
2024-03-09 18:23:19 +0100

    task1-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,9 +2,9 @@ const list = document.querySelector('#categories');
 const elems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${elems.length}`);
 elems.forEach(elem => {
-  showAnimalInfo(elem);
+  showElementsInfo(elem);
 });
-function showAnimalInfo(category) {
+function showElementsInfo(category) {
   let categoryName = category.querySelector('h2').textContent;
   let categoryElem = category.querySelectorAll('li').length;
   console.log(`Category: ${categoryName}`);
