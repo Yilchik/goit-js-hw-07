15edade396d5fbd6da49a1151180779209c3b3a2
Yilchik <yulyayulia2@gmail.com>
2024-03-10 17:17:14 +0100

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const button = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+
+button.addEventListener('click', function () {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  textColor.textContent = newColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
