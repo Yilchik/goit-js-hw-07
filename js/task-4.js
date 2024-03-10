8497481dc71609a41e7298dcb5dce5dc1b03401b
Yilchik <yulyayulia2@gmail.com>
2024-03-10 16:31:25 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const registerForm = document.querySelector('form');
+registerForm.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  }
+
+  const data = {
+    email: email,
+    password: password,
+  };
+
+  console.log(data);
+  registerForm.reset();
+}
