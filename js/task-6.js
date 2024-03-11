343a8941879c2e8348fa7d2fd746b660f4f1942d
Yilchik <yulyayulia2@gmail.com>
2024-03-11 21:30:54 +0100

    task

--- js/task-6.js
+++ js/task-6.js
@@ -3,7 +3,7 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const formDiv = document.querySelector('controls');
+const formDiv = document.querySelector('#controls');
 const boxDiv = document.querySelector('#boxes');
 
 const input = document.querySelector('input[type="number"]');
@@ -17,23 +17,25 @@ destroyBtn.classList.add('destroy');
 const makeBox = createBtn.addEventListener('click', createBoxes);
 
 function createBoxes(amount) {
+  amount = Number(input.value);
+  boxDiv.innerHTML = '';
   let size = 30;
   if (amount < 1 || amount > 100) {
     return;
   }
-  for (let i = 0; i <= amount; i++) {
+  for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
-    box.style.width = '${size}px';
-    box.style.height = '${size}px';
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
     boxDiv.append(box);
     size += 10;
   }
-  amount.innerHTML = '';
+  input.value = '';
 }
+
+const deleteBox = destroyBtn.addEventListener('click', destroyBoxes);
 function destroyBoxes() {
   boxDiv.innerHTML = '';
 }
-console.dir(createBtn);
-console.dir(input);
-console.dir(makeBox);
